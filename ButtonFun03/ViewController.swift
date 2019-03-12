e03dbef629019b6c7299b91e22f021cc596ca536
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-12 15:30:10 +0900

    2nd commit

--- ButtonFun03/ViewController.swift
+++ ButtonFun03/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        //print("Hello Button Event!")
+        myLabel.text = "Hello Button Event!"
+    }
+    
+    @IBAction func label_Clear(_ sender: Any) {
+        myLabel.text = ""
+    }
+    
+    @IBAction func changeViewColor(_ sender: Any) {
+        self.view.backgroundColor = UIColor.white
+    }
 }
 
